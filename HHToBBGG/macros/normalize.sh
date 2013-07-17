eeecadf841701f9850cb1d64168db1c7551941da
SiXie <Si.Xie@cern.ch>
2013-07-17 17:59:51 +0200

    update

--- HHToBBGG/macros/normalize.sh
+++ HHToBBGG/macros/normalize.sh
@@ -1,11 +1,13 @@
-root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/HHToBBGG/ntuples/HHToBBGGNtuple.HHtoBBGG-14tev-START53_V7A.root","HHtoBBGG-14tev-START53_V7A","/afs/cern.ch/work/s/sixie/public/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.HHtoBBGG-14tev-START53_V7A.normalized.root")'
+root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/ntuples/HHToBBGGNtuple.HHtoBBGG-14tev-START53_V7A.root","HHtoBBGG-14tev-START53_V7A","/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.HHtoBBGG-14tev-START53_V7A.normalized.root")'
 
-root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/HHToBBGG/ntuples/HHToBBGGNtuple.HHtoBBGG-8tev-START53_V7A.root","HHtoBBGG-8tev-START53_V7A","/afs/cern.ch/work/s/sixie/public/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.HHtoBBGG-8tev-START53_V7A.normalized.root")'
+root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/ntuples/HHToBBGGNtuple.HHtoBBGG-8tev-START53_V7A.root","HHtoBBGG-8tev-START53_V7A","/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.HHtoBBGG-8tev-START53_V7A.normalized.root")'
 
-root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/HHToBBGG/ntuples/HHToBBGGNtuple.ttHgg-125-START53_V7A.root","ttHgg-125-START53_V7A","/afs/cern.ch/work/s/sixie/public/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.ttHgg-125-START53_V7A.normalized.root")'
+root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/ntuples/HHToBBGGNtuple.ttHgg-125-START53_V7A.root","ttHgg-125-START53_V7A","/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.ttHgg-125-START53_V7A.normalized.root")'
 
-root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/HHToBBGG/ntuples/HHToBBGGNtuple.ZHgg-125-START53_V7A.root","ZHgg-125-START53_V7A","/afs/cern.ch/work/s/sixie/public/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.ZHgg-125-START53_V7A.normalized.root")'
+root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/ntuples/HHToBBGGNtuple.ZHgg-125-START53_V7A.root","ZHgg-125-START53_V7A","/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.ZHgg-125-START53_V7A.normalized.root")'
 
-root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/HHToBBGG/ntuples/HHToBBGGNtuple.ggHgg-125-START53_V7A.root","ggHgg-125-START53_V7A","/afs/cern.ch/work/s/sixie/public/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.ggHgg-125-START53_V7A.normalized.root")'
+root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/ntuples/HHToBBGGNtuple.ggHgg-125-START53_V7A.root","ggHgg-125-START53_V7A","/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.ggHgg-125-START53_V7A.normalized.root")'
 
 
+
+root -l -b -q CMSAna/HHToBBGG/macros/NormalizeNtuple.C+'("/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/ntuples/HHToBBGGNtuple.DiPhotonBB_M60To200_14TeV-BBGG.root","DiphotonBB_M60To200_14TeV-BBGG","/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.DiPhotonBB_M60To200_14TeV-BBGG.normalized.root")'
