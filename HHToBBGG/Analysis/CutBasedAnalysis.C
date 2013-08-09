e6106df5960b120633729db0e89a3a987218ea5d
SiXie <Si.Xie@cern.ch>
2013-08-09 15:56:54 +0200

    change colors. add samples

--- HHToBBGG/Analysis/CutBasedAnalysis.C
+++ HHToBBGG/Analysis/CutBasedAnalysis.C
@@ -9,13 +9,16 @@
 #include <vector>
 #include <map>
 
-Int_t bkgColors[7] = { kRed, kBlue,  kGreen+2, kMagenta, kCyan, kYellow, kBlack };
-string bkgLegendLabels[7] = { "HH#rightarrow bb#gamma#gamma", 
+Int_t bkgColors[10] = { kRed, kBlue,  kGreen+2, kMagenta, kCyan, kBlue-9, kBlack, kRed-6, kBlue-6, kMagenta+3 };
+string bkgLegendLabels[10] = { "HH#rightarrow bb#gamma#gamma", 
                              "ttH,H#rightarrow#gamma#gamma",
                              "ZH#rightarrow bb #gamma#gamma",
                              "bbH (ggH)",
                              "ttbar",
-                             "#gamma#gamma + jets",
+                             "#gamma#gamma bb",
+                             "#gamma#gamma jj",
+                             "bbjj",
+                             "ccjj",
                              "jjjj" };
 
 //*************************************************************************************************
@@ -40,7 +43,7 @@ TH1F* NormalizeHist(TH1F *originalHist) {
 //------------------------------------------------------------------------------
 // PlotHiggsRes_LP
 //------------------------------------------------------------------------------
-void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/public/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.main.root")
+void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/public/Phase2Upgrade/HHToBBGG/normalizedNtuples/HHToBBGGNtuple.main.root")
 {
 
   double intLumi = 3000; //in units of fb^-1
@@ -62,7 +65,7 @@ void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/pub
   vector<TH1F*> dibjetPt;
   vector<TH1F*> bbggMass;
 
-  for (UInt_t i = 1; i <= 7; ++i) {
+  for (UInt_t i = 1; i <= 10; ++i) {
     diphotonMass.push_back( new TH1F( Form("diphotonMass_%d",i), ";M_{#gamma#gamma} [GeV/c^{2}];Number of Events", 100, 100, 150));
     diphotonMass[i-1]->SetFillColor(bkgColors[i-1]);
     diphotonMass[i-1]->SetLineColor(bkgColors[i-1]);
@@ -119,7 +122,18 @@ void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/pub
     bbggMass[i-1]->SetStats(false);
 
  }
-        
+
+  //*******************************************************************************************
+  //Define CutFlow Event Counts
+  //*******************************************************************************************
+  vector<double> NEventsPassStage1;
+  vector<double> NEventsPassStage2;
+  vector<double> NEventsPassStage3;
+  for (UInt_t i = 1; i <= 10; ++i) {
+    NEventsPassStage1.push_back(0);
+    NEventsPassStage2.push_back(0);
+    NEventsPassStage3.push_back(0);
+  }
 
   //*******************************************************************************************
   //Do analysis
@@ -135,14 +149,16 @@ void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/pub
     double weight = event.weight*intLumi;
 
     //make sure the sample type makes sense
-     assert(event.sampletype >= 1 && event.sampletype <= 7);
+     assert(event.sampletype >= 1 && event.sampletype <= 10);
     
 //      if (event.sampletype != 1) continue;
 
      
-     if (event.bjet1.Pt() > 25.0 && event.bjet2.Pt() > 25.0
+     if (event.bjet1.Pt() > 30.0 && event.bjet2.Pt() > 30.0
          && event.pho1.Pt() > 25.0 && event.pho2.Pt() > 25.0
          && fmax(event.pho1.Pt(),event.pho2.Pt()) > 40.0
+         && fabs(event.pho1.Eta()) < 2.5 && fabs(event.pho2.Eta()) < 2.5
+         && fabs(event.bjet1.Eta()) < 2.4 && fabs(event.bjet2.Eta()) < 2.4
        ) {
 
        diphotonMass[event.sampletype-1]->Fill( event.diphoton.M() , weight);
@@ -150,8 +166,13 @@ void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/pub
        nlep[event.sampletype-1]->Fill( event.nlep , weight);
        njets[event.sampletype-1]->Fill( event.njets , weight);
        ncentraljets[event.sampletype-1]->Fill( event.ncentraljets , weight);
-       
-       if (event.nlep <= 0 && event.ncentraljets <= 3
+
+       if (event.diphoton.M() > 100 && event.diphoton.M() < 150
+           && event.dibjet.M() > 60 && event.dibjet.M() < 200 ) {
+         NEventsPassStage1[event.sampletype-1] += weight;
+       }
+
+       if (event.nlep <= 0 && event.ncentraljets <= 4
            && event.diphoton.M() > 120 && event.diphoton.M() < 130
            && event.dibjet.M() > 105 && event.dibjet.M() < 145
          ) {
@@ -159,11 +180,13 @@ void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/pub
          dRgg[event.sampletype-1]->Fill( event.DRgg , weight);
          dRbb[event.sampletype-1]->Fill( event.DRbb , weight);
          minDRgb[event.sampletype-1]->Fill( event.minDRgb , weight);
+         NEventsPassStage2[event.sampletype-1] += weight;
 
          if (event.DRgg < 2.0 && event.minDRgb > 1.0) {
            diphotonPt[event.sampletype-1]->Fill( event.diphoton.Pt(), weight);
            dibjetPt[event.sampletype-1]->Fill( event.dibjet.Pt(), weight);
            bbggMass[event.sampletype-1]->Fill( event.bbgg.M(), weight);
+           NEventsPassStage3[event.sampletype-1] += weight;
          }
 
        }
@@ -188,7 +211,9 @@ void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/pub
   legend->SetFillStyle(0);
 
   THStack *stackDiphotonMass = new THStack();
+  cout << "after object selection\n";
   for (Int_t i = diphotonMass.size()-1; i >= 0; i--) {
+    cout << "sample " << i << " : " << diphotonMass[i]->GetSumOfWeights() << "\n";
     if (diphotonMass[i]->Integral() > 0) {
       stackDiphotonMass->Add(diphotonMass[i]);
       legend->AddEntry(diphotonMass[i],bkgLegendLabels[i].c_str(), "F");
@@ -361,7 +386,9 @@ void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/pub
   legend->SetFillStyle(0);
 
   THStack *stackDRgg = new THStack();
+  cout << "After Mass windows + jet counting\n";
   for (Int_t i = dRgg.size()-1; i >= 0; i--) {
+    cout << "sample " << i << " : " << dRgg[i]->GetSumOfWeights() << "\n";
     if (dRgg[i]->Integral() > 0) {
       stackDRgg->Add(dRgg[i]);
       legend->AddEntry(dRgg[i],bkgLegendLabels[i].c_str(), "F");
@@ -521,8 +548,9 @@ void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/pub
   legend->SetFillStyle(0);
 
   THStack *stackdiphotonPt = new THStack();
+  cout << "after DR cuts\n";
   for (Int_t i = diphotonPt.size()-1; i >= 0; i--) {
-    cout << "sample " << i << " : " << diphotonPt[i]->Integral() << "\n";
+    cout << "sample " << i << " : " << diphotonPt[i]->GetSumOfWeights() << "\n";
     if (diphotonPt[i]->Integral() > 0) {
       stackdiphotonPt->Add(diphotonPt[i]);
       legend->AddEntry(diphotonPt[i],bkgLegendLabels[i].c_str(), "F");
@@ -574,5 +602,21 @@ void CutBasedAnalysis ( string InputFilename    = "/afs/cern.ch/work/s/sixie/pub
   stackbbggMass->GetHistogram()->GetXaxis()->SetTitle(((TH1F*)(stackbbggMass->GetHists()->At(0)))->GetXaxis()->GetTitle());
   legend->Draw();
   cv->SaveAs("bbggMass_AfterSelStep2.gif");
+
+
+
+
+  //*******************************************************************************************
+  //Show event count summary
+  //*******************************************************************************************
+  cout << "\n";
+  cout << "Event Counts\n";
+  for (UInt_t i = 0; i < 10; ++i) {
+    cout << bkgLegendLabels[i] << " : "
+         << NEventsPassStage1[i] << " " 
+         << NEventsPassStage2[i] << " " 
+         << NEventsPassStage3[i] << " " 
+         << "\n";
+  }
 
 }
