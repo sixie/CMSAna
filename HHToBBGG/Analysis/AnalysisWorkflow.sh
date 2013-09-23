c3c0a7fc5e490b01f183f9ef9a1943ed73881e41
SiXie <Si.Xie@cern.ch>
2013-09-23 16:21:01 +0200

    small fix

--- HHToBBGG/Analysis/AnalysisWorkflow.sh
+++ HHToBBGG/Analysis/AnalysisWorkflow.sh
@@ -31,7 +31,7 @@ root -l CMSAna/HHToBBGG/Analysis/PlotEfficiencies.C+'("CMSAna/HHToBBGG/data/Phot
  HHToBBGGEvent->Draw("diphoton.M()","3000*weight*(pho1.Pt()>25 && pho2.Pt()>25 && max(pho1.Pt(),pho2.Pt())>40 && abs(pho1.Eta())<2.5 && abs(pho2.Eta())<2.5 && bjet1.Pt()>30 && bjet2.Pt() > 30 && abs(bjet1.Eta())<2.4 && abs(bjet2.Eta())<2.4 && diphoton.M() > 120 && diphoton.M() < 130 && dibjet.M() > 105 && dibjet.M() < 145)");
 
 ##Cut Stage3
- HHToBBGGEvent->Draw("diphoton.Pt()","3000*weight*(pho1.Pt()>25 && pho2.Pt()>25 && max(pho1.Pt(),pho2.Pt())>40 && abs(pho1.Eta())<2.5 && abs(pho2.Eta())<2.5 && bjet1.Pt()>30 && bjet2.Pt() > 30 && abs(bjet1.Eta())<2.4 && abs(bjet2.Eta())<2.4 && diphoton.M() > 120 && diphoton.M() < 130 && dibjet.M() > 105 && dibjet.M() < 145 && DRgg < 2 && minDRgb>1)");
+ HHToBBGGEvent->Draw("diphoton.Pt()","3000*weight*(pho1.Pt()>25 && pho2.Pt()>25 && max(pho1.Pt(),pho2.Pt())>40 && abs(pho1.Eta())<2.5 && abs(pho2.Eta())<2.5 && bjet1.Pt()>30 && bjet2.Pt() > 30 && abs(bjet1.Eta())<2.4 && abs(bjet2.Eta())<2.4 && diphoton.M() > 120 && diphoton.M() < 130 && dibjet.M() > 105 && dibjet.M() < 145 && DRgg < 2 && minDRgb>1 && ncentraljets < 4)");
 
 
  HHToBBGGEvent->Draw("diphoton.M()","3000*weight*(pho1.Pt()>25 && pho2.Pt()>25 && abs(pho1.Eta())<2.5 && abs(pho2.Eta())<2.5 && bjet1.Pt()>30 && bjet2.Pt() > 30 && abs(bjet1.Eta())<2.4 && abs(bjet2.Eta())<2.4 && DRgg < 2 && minDRgb>1)");
